3899a29f3c40236cf1a85889909055a86f219b19
Caleb Lehman <lehman.346@osu.edu>
2019-10-24 07:56:58 -0400

    First round of testing

--- pa2/tests.sh
+++ pa2/tests.sh
@@ -1,9 +1,8 @@
 #PBS -N AMR-tests
-#PBS -l walltime=0:30:00
+#PBS -l walltime=6:00:00
 #PBS -l nodes=1:ppn=40
 #PBS -j oe
 #PBS -A PZS0711
-#PBS -q debug
 
 run_tests() {
   local test_file="$1"
@@ -20,6 +19,24 @@ run_tests() {
   done
 
   echo "Finished tests with file ${test_file}"
+}
+
+process_results() {
+  local test_file="$1"
+
+  echo "Processing results from file ${test_file}..."
+  test_name=$(basename ${test_file})
+  results_files=
+  for prog in ${programs[@]}; do
+    results_files="${results_files} ${test_name}_${prog}_results.txt"
+  done
+  plt_out_file=${test_name}_results.png
+  echo "python process_results.py ${test_name} ${plt_out_file} ${results_files}"
+  for prog in ${programs[@]}; do
+    results_file=${test_name}_${prog}_results.txt
+    mv ${results_file} ${PBS_O_WORKDIR}/results/.
+  done
+  echo "mv ${plt_out_file} ${PBS_O_WORKDIR}/results/."
 }
 
 # Load modules
@@ -41,20 +58,5 @@ mkdir -p ${PBS_O_WORKDIR}/results
 
 for test_file in ${test_files[@]}; do
   run_tests ${test_file}
-done
-
-# Process results
-for test_file in ${test_files[@]}; do
-  test_name=$(basename ${test_file})
-  results_files=
-  for prog in ${programs[@]}; do
-    results_files="${results_files} ${test_name}_${prog}_results.txt"
-  done
-  plt_out_file=${test_name}_results.png
-  echo "python process_results.py ${test_name} ${plt_out_file} ${results_files}"
-  for prog in ${programs[@]}; do
-    results_file=${test_name}_${prog}_results.txt
-    mv ${results_file} ${PBS_O_WORKDIR}/results/.
-  done
-  echo "mv ${plt_out_file} ${PBS_O_WORKDIR}/results/."
+  process_results ${test_file}
 done
